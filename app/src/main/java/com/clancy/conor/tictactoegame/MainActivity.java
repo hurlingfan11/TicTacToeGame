cc79e78bceddf126c01aa9eca9dab28d53a13b75
hurlingfan11 <39068491+hurlingfan11@users.noreply.github.com>
2018-10-01 23:03:39 +0100

    XML completed

--- app/src/main/java/com/clancy/conor/tictactoegame/MainActivity.java
+++ app/src/main/java/com/clancy/conor/tictactoegame/MainActivity.java
@@ -2,6 +2,9 @@ package com.clancy.conor.tictactoegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGame = new  TicTacToeGame(this);
+    }
+
+    public void pressedSquare(View view){
+
+    String tagAsStr =view.getTag().toString();
+    int tagAsInt = Integer.parseInt(tagAsStr);
+    Log.d( "TTT", "You Pressed index " + tagAsInt);
+
+    // Log.d outputs text to Logcat
+
+    Toast.makeText(this, "You Pressed index" + tagAsInt, Toast.LENGTH_SHORT).show();
+    // Toast outputs text to screen of device
+
+    }
+
+    public void pressedNewGame(View view){
+
+        String tagAsStr =view.getTag().toString();
+        int tagAsInt = Integer.parseInt(tagAsStr);
+        //Log.d( "TTT", "You Pressed New Game " + tagAsInt);
+
+        // Log.d outputs text to Logcat
+
+        Toast.makeText(this, "You Pressed New Game", Toast.LENGTH_SHORT).show();
+        // Toast outputs text to screen of device
+
     }
 }
